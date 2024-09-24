19d2d0170e61a80bea7dc2832c55519dcd89da0e
Harikishan bairwa <harrybairwa51@gmail.com>
2024-09-24 18:15:08 +0530

    fgfg

---
+++ src/Controller/Crm/HomeController.php
@@ -0,0 +1,12 @@
+<?php
+    namespace App\Controller\Crm;
+
+    use App\Controller\AppController;
+
+    class HomeController extends AppController
+    {
+        public function index()
+        {
+
+        }
+    }
---
+++ templates/layout/crm.php
@@ -0,0 +1,4 @@
+<?=$this->element('crm/header');?>
+<?=$this->fetch('content');?>
+<?=$this->element('crm/footer');?>
+<?=$this->element('crm/sidebar');?>
